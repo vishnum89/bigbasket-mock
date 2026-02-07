d421ecbfac3e161291d20adbd2be917c9bfb6e2e
Vishnu Madhusudanan <vishnumadhusudanan@MacBookPro.lan>
2026-02-07 12:36:07 +0530

    new routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/admin/login/login';
+import { Layout } from './pages/admin/layout/layout';
+import { Products } from './pages/admin/products/products';
 
 export const routes: Routes = [
     {
@@ -10,5 +12,15 @@ export const routes: Routes = [
     {
         path: 'login',
         component: Login
+    },
+    {
+        path: '',
+        component: Layout,
+        children:[
+            {
+                path:'products',
+                component:Products
+            }
+        ]
     }
 ];
--- src/app/pages/admin/login/login.ts
+++ src/app/pages/admin/login/login.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { User } from '../../../models/user';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, RouterModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+
+constructor(private router : Router){}
+
+user : User = {
+    username : '',
+    password : ''
+  }
+
+onLogin() {
+  if(this.user.username == 'admin' && this.user.password == '12345'){
+  this.router.navigateByUrl('/products')
+  }else{
+    alert('Wrong Credentials!!')
+  }
+}
+
+
 
 }
