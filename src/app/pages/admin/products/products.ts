6b12af0808eed1bc0b0844c12af520c411b5df4c
Vishnu Madhusudanan <vishnumadhusudanan@MacBookPro.lan>
2026-02-07 15:12:51 +0530

    styling

--- src/app/pages/admin/products/products.ts
+++ src/app/pages/admin/products/products.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
 export class Products {
+    isSidePanelVisible: boolean = false;
 
+    viewSidePanel(){
+      this.isSidePanelVisible = true;
+    }
+
+    closeSidePanel(){
+      this.isSidePanelVisible = false;
+    }
 }
